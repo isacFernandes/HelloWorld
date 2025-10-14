e5526394c6a38f58abeedface8d5b8d41d5a052a
isacFernandes <145605524+isacFernandes@users.noreply.github.com>
2025-10-14 17:52:22 -0300

    Informações extras

---
+++ Operadores/app.py
@@ -0,0 +1,29 @@
+"""
++ soma
+- subtração
+* multiplicação
+/ divisão
+// divisão arredondada
+% resto da divisão
+** expoente
+
+"""
+x = 10 
+# x = x + 3
+x += 3
+
+print(x)
+
+
+# Operadores de comparação retornam valor booleano
+"""
+> maior que
+>= maior ou igual
+< menor que
+<= menor ou igual
+== igual 
+!= não igual
+
+"""
+
+# Operadores lógicos
