d8f3d84c7fda3bd3ea97e824697803ac7c690ee5
isacFernandes <145605524+isacFernandes@users.noreply.github.com>
2025-10-14 04:00:37 -0300

    Primeiros passos

---
+++ Type Conversion/app.py
@@ -0,0 +1,12 @@
+# int() converte o valor em um número inteiro
+# float() converte o valor em um número decimal
+# bool() converte o valor em um booleano
+# str() converte o valor em uma string
+
+# Calculadora de soma simples
+primeiro = float(input("Digite o primeiro número: "))
+segundo = float(input("Digite o segundo número: "))
+soma = primeiro + segundo
+
+print("Resultado: " + str(soma))
+ 
---
+++ Variaveis/app.py
@@ -0,0 +1,6 @@
+# Exercício de variáveis canal Programming with Mosh
+# Checkin de hospital
+
+patient_name = "John Smith"
+age = 20
+new_patient = True
