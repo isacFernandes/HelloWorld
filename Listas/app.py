cf4740e41961fb1a190784e674972208ad4c1315
isacFernandes <145605524+isacFernandes@users.noreply.github.com>
2025-11-07 07:20:53 -0300

    Listas

---
+++ Listas/app.py
@@ -0,0 +1,4 @@
+nomes = ["João", "Maria", "Enzo", "Valentina", "Laira"]
+nomes[2] = "Isa"
+print(nomes[0:3])
+print(nomes)
---
+++ Métodos de Lista/app.py
@@ -0,0 +1,14 @@
+
+'''
+ .append > adiciona um item no final da lista
+ .insert > adiciona um item em qualquer lugar da lista
+ .remove > remove um item da lista
+ .clear > remove todos os itens da lista
+ in > verifica se um item está na lista
+ len > retorna o número de itens da lista
+'''
+
+
+
+numeros = [1, 2, 3, 4, 5]
+print(len(numeros))
